2b57be23451e790132cc96e3abd15f6d29bfbabb
Guilherme Bley <guilhermecbley@gmail.com>
2024-01-02 22:36:28 -0300

    create entity best model

---
+++ src/core/BenchmarkCar.Domain/Entities/RecordEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace BenchmarkCar.Domain.Entities;
+
+public record RecordEntity : IEntity
+{
+    public virtual Guid EntityId { get; } = Guid.NewGuid();
+
+    public RecordEntity()
+        => ValidEntity();
+
+    protected virtual void ValidEntity()
+    {
+    }
+}
---
+++ src/core/BenchmarkCar.Domain/Entities/Vehicles/BestModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BenchmarkCar.Domain.Entities.Vehicles;
+
+public record BestModel(
+    Guid Id,
+    VehicleModel Vehicle,
+    string Area,
+    DateTimeOffset InsertedAt)
+    : RecordEntity
+{
+    protected override void ValidEntity()
+    {
+        base.ValidEntity();
+
+        if (Area.Length > 255)
+            throw new CommonCoreException("Invalid 'area' length.");
+    }
+
+    public static BestModel CreateNow(
+        VehicleModel Vehicle,
+        string Area)
+        => new BestModel(
+            Guid.NewGuid(),
+            Vehicle,
+            Area.Trim('\n', '\t', ' ').ToUpperInvariant(),
+            DateTimeOffset.UtcNow);
+}
