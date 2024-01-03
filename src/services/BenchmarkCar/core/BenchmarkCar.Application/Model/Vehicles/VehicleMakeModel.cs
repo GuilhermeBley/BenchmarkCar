1f85e165b0a30604f50af6d29010569357fb5d4e
Guilherme Bley <guilhermecbley@gmail.com>
2024-01-02 22:56:50 -0300

    VehicleMakeModel

---
+++ src/services/BenchmarkCar/core/BenchmarkCar.Application/Model/Vehicles/VehicleMakeModel.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkCar.Domain.Entities.Vehicles;
+
+namespace BenchmarkCar.Application.Model.Vehicles;
+
+public  class VehicleMakeModel
+{
+    public Guid Id { get; set; }
+    public string NormalizedName { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string ExternalId { get; set; } = string.Empty;
+    public DateTimeOffset InsertedAt { get; set; }
+
+    public static VehicleMakeModel MapFromEntity(VehicleMake entity)
+        => new()
+        {
+            Id = entity.Id,
+            ExternalId = entity.ExternalId.ToString() ?? string.Empty,
+            NormalizedName = entity.NormalizedName,
+            Name = entity.Name,
+            InsertedAt = entity.InsertedAt
+        };
+}
