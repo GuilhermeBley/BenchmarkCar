3bcad578210152c5bb09d8f35327bc934c4f6d91
Guilherme Bley <guilhermecbley@gmail.com>
2024-01-02 23:05:52 -0300

    BestModelModel

---
+++ src/services/BenchmarkCar/core/BenchmarkCar.Application/Model/Vehicles/BestModelModel.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkCar.Domain.Entities.Vehicles;
+
+namespace BenchmarkCar.Application.Model.Vehicles;
+
+public class BestModelModel
+{
+    public Guid Id { get; set; }
+    public Guid VehicleModelId { get; set; }
+    public string Area { get; set; } = string.Empty;
+    public DateTimeOffset InsertedAt { get; set; }
+
+    public static BestModelModel MapFromEntity(BestModel entity)
+        => new()
+        {
+            Id = entity.Id,
+            InsertedAt = entity.InsertedAt,
+            Area = entity.Area,
+            VehicleModelId = entity.Vehicle.Id
+        };
+}
