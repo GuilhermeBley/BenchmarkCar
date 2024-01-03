ad61f36e40dc4df367eedc28309722ded6a8a31d
Guilherme Bley <guilhermecbley@gmail.com>
2024-01-03 18:48:32 -0300

    add context

---
+++ src/services/BenchmarkCar/core/BenchmarkCar.Application/Repositories/IVehicleContext.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkCar.Application.Model.Vehicles;
+using Microsoft.EntityFrameworkCore;
+
+namespace BenchmarkCar.Application.Repositories;
+
+public interface IVehicleContext
+{
+    DbSet<VehicleMakeModel> VehiclesMakes { get; }
+    DbSet<VehicleModelModel> VehiclesModels { get; }
+    DbSet<BestModelModel> BestModels { get; }
+    DbSet<ModelBodyModel> ModelBodies { get; }
+    DbSet<ModelEngineModel> EngineModels { get; }
+}
