d35086c20d357ca34a3c125ae5250a8b268b5c0b
Guilherme Bley <guilhermecbley@gmail.com>
2024-01-09 22:33:33 -0300

    add OnModelCreating

--- src/services/BenchmarkCar/infrastructure/BenchmarkCar.Infrastructure/Repositories/MySqlVehicleContext.cs
+++ src/services/BenchmarkCar/infrastructure/BenchmarkCar.Infrastructure/Repositories/MySqlVehicleContext.cs
@@ -42,4 +42,42 @@ internal class MySqlVehicleContext
             {
             });
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<VehicleMakeModel>(cfg =>
+        {
+            cfg.HasKey(p => p.Id);
+            cfg.HasIndex(p => p.NormalizedName).IsUnique();
+        });
+
+        modelBuilder.Entity<VehicleModelModel>(cfg =>
+        {
+            cfg.HasKey(p => p.Id);
+            cfg.HasIndex(p => p.NormalizedName).IsUnique();
+            cfg.HasOne(p => p.VehicleMake).WithMany().HasForeignKey(p => p.VehicleMakeId);
+        });
+
+        modelBuilder.Entity<BestModelModel>(cfg =>
+        {
+            cfg.HasKey(p => p.Id);
+            cfg.HasOne<VehicleModelModel>().WithMany().HasForeignKey(p => p.VehicleModelId);
+        });
+
+        modelBuilder.Entity<ModelBodyModel>(cfg =>
+        {
+            cfg.HasKey(p => p.ModelId);
+            cfg.HasIndex(p => p.ExternalId).IsUnique();
+            cfg.HasOne<VehicleModelModel>().WithMany().HasForeignKey(p => p.ModelId);
+        });
+
+        modelBuilder.Entity<ModelEngineModel>(cfg =>
+        {
+            cfg.HasKey(p => p.ModelId);
+            cfg.HasIndex(p => p.ExternalId).IsUnique();
+            cfg.HasOne<VehicleModelModel>().WithMany().HasForeignKey(p => p.ModelId);
+        });
+    }
 }
