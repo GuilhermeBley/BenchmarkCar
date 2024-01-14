df85dbff420141ac6093c65ba0f4419896887681
Guilherme Bley <guilhermecbley@gmail.com>
2024-01-14 16:00:53 -0300

    add login to CarApiVehiclesDataQuery

--- src/services/BenchmarkCar/infrastructure/BenchmarkCar.Infrastructure/ExternalApi/CarApiVehiclesDataQuery.cs
+++ src/services/BenchmarkCar/infrastructure/BenchmarkCar.Infrastructure/ExternalApi/CarApiVehiclesDataQuery.cs
@@ -1,5 +1,10 @@
 ﻿using BenchmarkCar.Application.ExternalApi;
 using BenchmarkCar.Application.IntegrationEvents.ModelRequestedToSearc;
+using BenchmarkCar.Infrastructure.Options;
+using Microsoft.Extensions.Options;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http;
+using System.Net.Http.Json;
 
 namespace BenchmarkCar.Infrastructure.ExternalApi;
 
@@ -8,10 +13,134 @@ internal class CarApiVehiclesDataQuery
 {
     private const string URL = "https://carapi.app/";
 
-    public Task<CreateVehicleModelApiDetails> GetByExternalModelId(
+    public readonly static Action<IServiceProvider, HttpClient> Configure =
+        (provider, client) =>
+        {
+            client.BaseAddress = new Uri(URL);
+            client.Timeout = new TimeSpan(0, 5, 0);
+        };
+    private readonly HttpClient _httpClient;
+    private readonly IOptions<CarApiOptions> _options;
+
+    /// <summary>
+    /// Lock all the requests
+    /// </summary>
+    /// <remarks>
+    ///     <para>It's needed to avoid cross-origin</para>
+    /// </remarks>
+    private static SemaphoreSlim _lockRequest
+        = new(1, 1);
+
+    private static SemaphoreSession _semaphoreSession
+        => new(_lockRequest);
+
+    private LoginSession _loginSession
+        => new(_httpClient, _options);
+
+    public CarApiVehiclesDataQuery(
+        HttpClient httpClient,
+            IOptions<CarApiOptions> options)
+    {
+        _httpClient = httpClient;
+        _options = options;
+    }
+
+    public async Task<CreateVehicleModelApiDetails> GetByExternalModelId(
         object modelId, 
         CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        if (!(modelId is string modelTextId))
+            throw new CommonCoreException("Invalid id type.");
+
+        using var session = _semaphoreSession;
+
+        await session.WaitAsync(cancellationToken);
+
+        await _loginSession.EnsureClientLoggedAsync(cancellationToken);
+
+
+    }
+
+    /// <summary>
+    /// Manages login from current client
+    /// </summary>
+    private class LoginSession
+    {
+        private string? _lastToken;
+        private HttpClient _httpClient;
+        private readonly IOptions<CarApiOptions> _options;
+
+        public LoginSession(
+            HttpClient httpClient,
+            IOptions<CarApiOptions> options)
+        {
+            _httpClient = httpClient;
+            _options = options;
+        }
+
+        public async Task EnsureClientLoggedAsync(CancellationToken cancellationToken = default)
+        {
+            var token = await GetTokenAsync(cancellationToken);
+
+            _httpClient.DefaultRequestHeaders.Authorization 
+                = new System.Net.Http.Headers.AuthenticationHeaderValue(
+                    "Bearer",
+                    token);
+        }
+
+        private async Task<string> GetTokenAsync(CancellationToken cancellationToken = default)
+        {
+            const string PATH = "/auth/login";
+
+            if (_lastToken is not null &&
+                IsntExpired(_lastToken))
+                return _lastToken;
+
+            using var result = await _httpClient.PostAsJsonAsync(
+                PATH, 
+                new
+                {
+                    api_token = _options.Value.ApiToken,
+                    api_secret = _options.Value.ApiSecret
+                },
+                cancellationToken);
+
+            result.EnsureSuccessStatusCode();
+            
+            return await result.Content.ReadAsStringAsync();
+        }
+
+        private static bool IsntExpired(string token)
+            => !IsExpired(token);
+
+        private static bool IsExpired(string token)
+        {
+            var jwtHandler = new JwtSecurityTokenHandler();
+            var jsonToken = jwtHandler.ReadToken(token) as JwtSecurityToken;
+
+            if (jsonToken == null || jsonToken.ValidTo < DateTime.UtcNow.AddHours(-1))
+            {
+                return true; // Token is expired
+            }
+
+            return false;
+        }
+    }
+
+    private class SemaphoreSession
+        : IDisposable
+    {
+        private readonly SemaphoreSlim _semaphore;
+
+        public SemaphoreSession(SemaphoreSlim semaphore)
+        {
+            _semaphore = semaphore;
+        }
+
+        public Task WaitAsync(CancellationToken cancellationToken = default)
+            => _semaphore.WaitAsync(cancellationToken);
+
+        public void Dispose()
+            => _semaphore.Release();
     }
 }
---
+++ src/services/BenchmarkCar/infrastructure/BenchmarkCar.Infrastructure/Options/CarApiOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace BenchmarkCar.Infrastructure.Options;
+
+internal class CarApiOptions
+{
+    public const string SECTION = "CarApiOptions";
+
+    public string ApiToken { get; set; } = string.Empty;
+    public string ApiSecret { get; set; } = string.Empty;
+}
