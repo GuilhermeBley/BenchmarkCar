2f76a366e60799391623f7e21781252108786ae1
Guilherme Bley <guilhermecbley@gmail.com>
2024-01-14 00:01:29 -0300

    add extension methods

---
+++ src/services/BenchmarkCar/infrastructure/BenchmarkCar.Infrastructure/Extensions/ApplicationServicesDiExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BenchmarkCar.Infrastructure.Extensions;
+
+public static class ApplicationServicesDiExtension
+{
+    public static IServiceCollection AddApplicationServices(
+        this IServiceCollection serviceCollection,
+        IConfiguration configuration)
+    {
+        serviceCollection.AddDbContextPool<Repositories.SqlVehicleContext>(opt =>
+        {
+        });
+
+        serviceCollection.AddScoped<Application.Repositories.VehicleContext, Repositories.SqlVehicleContext>();
+
+        serviceCollection.AddSingleton<Application.Log.ICoreLogger, Log.InfrastructureLogger>();
+        serviceCollection.AddSingleton(typeof(Application.Log.ICoreLogger<>), typeof(Log.InfrastructureLogger<>));
+
+        return serviceCollection;
+    }
+}
---
+++ src/services/BenchmarkCar/infrastructure/BenchmarkCar.Infrastructure/Log_/InfrastructureLogger.cs
@@ -0,0 +1,57 @@
+﻿using BenchmarkCar.Application.Log;
+using Microsoft.Extensions.Logging;
+
+namespace BenchmarkCar.Infrastructure.Log;
+
+internal class InfrastructureLogger
+    : ICoreLogger
+{
+    private readonly ILogger _logger;
+
+    public InfrastructureLogger(ILoggerFactory factory)
+    {
+        _logger = factory.CreateLogger("Default");
+    }
+
+    public void LogCritical(string? message, params object?[] args)
+        => _logger.LogCritical(message, args);
+
+    public void LogError(string? message, params object?[] args)
+        => _logger.LogError(message, args);
+
+    public void LogInformation(string? message, params object?[] args)
+        => _logger.LogInformation(message, args);
+
+    public void LogTrace(string? message, params object?[] args)
+        => _logger.LogTrace(message, args);
+
+    public void LogWarning(string? message, params object?[] args)
+        => _logger.LogWarning(message, args);
+}
+
+internal class InfrastructureLogger<T>
+    : ICoreLogger<T>
+{
+    private readonly ILogger _logger;
+
+    public InfrastructureLogger(
+        ILoggerFactory factory)
+    {
+        _logger = factory.CreateLogger<T>();
+    }
+
+    public void LogCritical(string? message, params object?[] args)
+        => _logger.LogCritical(message, args);
+
+    public void LogError(string? message, params object?[] args)
+        => _logger.LogError(message, args);
+
+    public void LogInformation(string? message, params object?[] args)
+        => _logger.LogInformation(message, args);
+
+    public void LogTrace(string? message, params object?[] args)
+        => _logger.LogTrace(message, args);
+
+    public void LogWarning(string? message, params object?[] args)
+        => _logger.LogWarning(message, args);
+}
