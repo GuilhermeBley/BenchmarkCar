ebe842f5c87c14652933e952b393c16129aeb02f
Guilherme Bley <guilhermecbley@gmail.com>
2024-01-21 01:09:04 -0300

    Create model resume #3 - CreateModelsByMakeHandler

---
+++ src/services/BenchmarkCar/core/BenchmarkCar.Application/IntegrationEvents/CreateModelsByMake/CreateModelsByMakeHandler.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkCar.Application.ExternalApi;
+using BenchmarkCar.EventBus.Abstractions;
+using BenchmarkCar.EventBus.Events;
+
+namespace BenchmarkCar.Application.IntegrationEvents.CreateModelsByMake;
+
+public class CreateModelsByMakeHandler
+    : IIntegrationEventHandler<CreateModelsByMakeIntegrationEvent>
+{
+    private readonly IVehiclesDataQuery _api;
+    private readonly ICoreLogger<CreateModelsByMakeHandler> _logger;
+
+    public CreateModelsByMakeHandler(
+        IVehiclesDataQuery api,
+        ICoreLogger<CreateModelsByMakeHandler> logger)
+    {
+        _api = api;
+        _logger = logger;
+    }
+
+    public Task Handle(
+        CreateModelsByMakeIntegrationEvent @event, 
+        CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
