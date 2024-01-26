50bac9e881dd1e736bcc42dd0fc890126775703b
Guilherme Bley <guilhermecbley@gmail.com>
2024-01-25 22:32:32 -0300

    add controllers

---
+++ src/services/BenchmarkCar/ui/BenchmarkCar.Ui.Api/Controllers/VehicleMakeController.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BenchmarkCar.Ui.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class VehicleMakeController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly ILogger<VehicleModelController> _logger;
+
+    public VehicleMakeController(
+        IMediator mediator,
+        ILogger<VehicleModelController> logger)
+    {
+        _mediator = mediator;
+        _logger = logger;
+    }
+
+    [HttpPost("Request")]
+    public async Task<ActionResult> RequestToCreateOrUpdateMakesAsync(
+        CancellationToken cancellationToken = default)
+    {
+        _logger.LogTrace("Requesting make creation at {0}.", DateTimeOffset.UtcNow);
+
+        var result = await _mediator.Send(
+            new Application.Commands.RequestVehicleMakeCreation.RequestVehicleMakeCreationRequest(),
+            cancellationToken);
+
+        return Ok(result);
+    }
+}
---
+++ src/services/BenchmarkCar/ui/BenchmarkCar.Ui.Api/Controllers/VehicleModelController.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BenchmarkCar.Ui.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class VehicleModelController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly ILogger<VehicleModelController> _logger;
+
+    public VehicleModelController(
+        IMediator mediator, 
+        ILogger<VehicleModelController> logger)
+    {
+        _mediator = mediator;
+        _logger = logger;
+    }
+
+    [HttpGet("Vehicle-Data/{id}")]
+    public async Task<ActionResult> GetVehicleCompleteDataAsync(
+        Guid id,
+        CancellationToken cancellationToken = default)
+    {
+        _logger.LogTrace("Requesting vehicle complete data '{0}' at {1}.", 
+            id, DateTimeOffset.UtcNow);
+
+        var result = await _mediator.Send(
+            new Application.Commands.GetVehicleCompleteData.GetVehicleCompleteDataRequest(id),
+            cancellationToken);
+
+        return Ok(result);
+    }
+}
