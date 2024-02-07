286988d5496561990d8d11eb240704df18e47d7f
Guilherme Bley <guilhermecbley@gmail.com>
2024-02-06 22:51:01 -0300

    Get comparative data model #9 - add migrations

---
+++ src/services/BenchmarkCar/BenchmarkCar.Ui.Api/Migrations/20240207014919_add ProcessingQueues.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BenchmarkCar.Ui.Api.Migrations
+{
+    public partial class addProcessingQueues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProcessingQueues",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Code = table.Column<int>(type: "int", nullable: false),
+                    Percent = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    Area = table.Column<string>(type: "varchar(255)", nullable: false),
+                    Key = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProcessingQueues", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProcessingQueues");
+        }
+    }
+}
