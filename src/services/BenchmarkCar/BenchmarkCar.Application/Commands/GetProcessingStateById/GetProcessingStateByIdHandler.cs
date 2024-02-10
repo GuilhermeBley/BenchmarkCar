c7af70ac6acb28a06901f4c7af96e4251b05d187
Guilherme Bley <guilhermecbley@gmail.com>
2024-02-09 23:31:51 -0300

    Get comparative data model #9 - get processing state by id handler

--- src/services/BenchmarkCar/BenchmarkCar.Application/Commands/GetProcessingStateById/GetProcessingStateByIdHandler.cs
+++ src/services/BenchmarkCar/BenchmarkCar.Application/Commands/GetProcessingStateById/GetProcessingStateByIdHandler.cs
@@ -1,14 +1,43 @@
-﻿using MediatR;
+﻿using BenchmarkCar.Application.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace BenchmarkCar.Application.Commands.GetProcessingStateById;
 
 public class GetProcessingStateByIdHandler
     : IRequestHandler<GetProcessingStateByIdRequest, GetProcessingStateByIdResponse>
 {
-    public Task<GetProcessingStateByIdResponse> Handle(
+    private readonly ICoreLogger<GetProcessingStateByIdHandler> _logger;
+    private readonly BenchmarkVehicleContext _context;
+
+    public GetProcessingStateByIdHandler(
+        ICoreLogger<GetProcessingStateByIdHandler> logger, 
+        BenchmarkVehicleContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    public async Task<GetProcessingStateByIdResponse> Handle(
         GetProcessingStateByIdRequest request, 
         CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var processingFound = 
+            await _context
+                .ProcessingQueues
+                .AsNoTracking()
+                .FirstOrDefaultAsync(e => e.Id == request.ProcessId, cancellationToken);
+
+        _logger.LogInformation("Processing '{0}' wasn't found.", request.ProcessId);
+
+        if (processingFound is null)
+            throw new NotFoundCoreException("Proccess not found.");
+
+        _logger.LogInformation("Processing '{0}' sucessfully found.", processingFound.Id);
+
+        return new GetProcessingStateByIdResponse(
+            processingFound.Id,
+            processingFound.Code,
+            processingFound.Percent);
     }
 }
