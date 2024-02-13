fa3c8f5cb8471f82bdecac05d3603ce8077d5b5a
Guilherme Bley <guilhermecbley@gmail.com>
2024-02-12 22:05:02 -0300

    Get comparative data model #9 - migrations to property kind to entity EF core

---
+++ src/services/BenchmarkCar/BenchmarkCar.Ui.Api/Migrations/20240213010342_meta data kind for ProcessingState.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BenchmarkCar.Ui.Api.Migrations
+{
+    public partial class metadatakindforProcessingState : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MetaData",
+                table: "ProcessingQueues",
+                type: "VARCHAR(MAX)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MetaData",
+                table: "ProcessingQueues");
+        }
+    }
+}
