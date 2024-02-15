87c8a154fd73a536ca6a666fdc66595d18be9c98
Guilherme Bley <guilhermecbley@gmail.com>
2024-02-14 21:41:37 -0300

    Model

---
+++ src/services/BenchmarkCar/BenchmarkCar.Application/Model/Queue/ProcessingResultModel.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkCar.Domain.Entities.Queue;
+using System.Text.Json;
+
+namespace BenchmarkCar.Application.Model.Queue;
+
+public class ProcessingResultModel
+{
+    public Guid LinkedProccessId { get; set; }
+    public string? Data { get; set; }
+    public DateTimeOffset ExpirationDate { get; set; }
+
+    public T? TryParseData<T>() 
+        where T : class
+    {
+        try
+        {
+            if (string.IsNullOrWhiteSpace(Data))
+                return null;
+
+            return JsonSerializer.Deserialize<T>(Data);
+        }
+        catch
+        {
+            return null;
+        }
+    }
+    
+    public static ProcessingResultModel MapFrom(
+        ProcessingResult entity)
+    {
+        return new ProcessingResultModel
+        {
+            Data = entity.Data is null ?
+                null :
+                JsonSerializer.Serialize(entity.Data),
+            ExpirationDate = entity.ExpirationDate,
+            LinkedProccessId = entity.LinkedProccessId
+        };
+    }
+}
