88dd048384666981bfb0938e2815f1598a178e04
Guilherme Bley <guilhermecbley@gmail.com>
2024-02-19 19:15:01 -0300

    add migrations to change

---
+++ src/services/BenchmarkCar/BenchmarkCar.Ui.Api/Migrations/20240219221312_add engine size property.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BenchmarkCar.Ui.Api.Migrations
+{
+    public partial class addenginesizeproperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MetaData",
+                table: "ProcessingQueues",
+                type: "VARCHAR(MAX)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(MAX)");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "EngineSize",
+                table: "EngineModels",
+                type: "decimal(10,2)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EngineSize",
+                table: "EngineModels");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MetaData",
+                table: "ProcessingQueues",
+                type: "VARCHAR(MAX)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(MAX)",
+                oldNullable: true);
+        }
+    }
+}
