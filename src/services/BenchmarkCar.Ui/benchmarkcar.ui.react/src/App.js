9fbda65912a425c62431abd0be2c613c08e1734f
Guilherme Bley <guilhermecbley@gmail.com>
2024-02-26 22:25:30 -0300

    Create react routes #13 - app routes

--- src/services/BenchmarkCar.Ui/benchmarkcar.ui.react/src/App.js
+++ src/services/BenchmarkCar.Ui/benchmarkcar.ui.react/src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import {Route, Switch} from 'react-router-dom'
-import { Component } from 'react';
+import { Route, Routes } from 'react-router-dom'
+import React, {Component} from 'react'
+
+import Home from './Pages/Home'
+import NotFound from './Pages/NotFound'
 
 class App extends Component {
   
   render() {
     return (
       <div className="App">
-        <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route component={NotFound}/>
-        </Switch>
+        <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route path='*' exact={true} element={<NotFound/>}/>
+        </Routes>
       </div>
     );
   }
--- src/services/BenchmarkCar.Ui/benchmarkcar.ui.react/src/Pages/NotFound/index.js
+++ src/services/BenchmarkCar.Ui/benchmarkcar.ui.react/src/Pages/NotFound/index.js
@@ -3,10 +3,12 @@ import React, { Component } from "react";
 class NotFound extends Component{
 
     render() {
-        <div className="not-found">
-            <h1>404</h1>
-            <h3>Page not found</h3>
-        </div>
+        return (
+            <div className="not-found">
+                <h1>404</h1>
+                <h3>Page not found</h3>
+            </div>
+        )
     }
 }
 
