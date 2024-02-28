90d471452a6cf89b86ce846a864da6d25737ca44
Guilherme Bley <guilhermecbley@gmail.com>
2024-02-27 23:34:49 -0300

    Use translation #15

--- src/services/BenchmarkCar.Ui/benchmarkcar.ui.react/src/Pages/Home/index.js
+++ src/services/BenchmarkCar.Ui/benchmarkcar.ui.react/src/Pages/Home/index.js
@@ -1,9 +1,27 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import { withTranslation } from 'react-i18next';
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+      }
+
+    componentDidMount() {
+        document.title = "Home";
+    }
+
     render() {
-        return (<h1>Home page</h1>);
+
+        const { t } = this.props;
+
+        return (
+            <div>
+                <h1>{t('home-title')}</h1>
+
+                
+            </div>
+        );
     }    
 }
 
-export default Home;
+export default withTranslation()(Home);
---
+++ src/services/BenchmarkCar.Ui/benchmarkcar.ui.react/src/i18n.js
@@ -0,0 +1,25 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import EN from './locales/en/translation.json';
+
+const resources = {
+  en: {
+    translation: EN
+  }
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    debug: true,
+
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    }
+  });
+
+
+export default i18n;
