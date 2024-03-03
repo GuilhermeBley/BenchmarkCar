b49cad744c225f2efe446da444ffab2d2563e350
Guilherme Bley <guilhermecbley@gmail.com>
2024-03-02 23:47:29 -0300

    Create react home page #14- configure axios

--- src/services/BenchmarkCar.Ui/benchmarkcar.ui.react/src/api/axiosBenc.js
+++ src/services/BenchmarkCar.Ui/benchmarkcar.ui.react/src/api/axiosBenc.js
@@ -1,13 +1,21 @@
 import axios from 'axios';
 
-const apiUrl = process.env.BENCHMARKCAR_URL;
 
-const axiosBenc = axios.create({
-  baseURL: apiUrl,
-  timeout: 5000,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+const createApiBench = () =>{
+
+  const apiUrl = process.env.BENCHMARKCAR_URL;
+  
+  console.log('Benchmark API: ' + apiUrl);
+
+  return axios.create({
+    baseURL: apiUrl,
+    timeout: 5000,
+    headers: {
+      'Content-Type': 'application/json',
+      },
+    });
+}
+
+const axiosBenc = createApiBench();
 
 export default axiosBenc;
--- src/services/BenchmarkCar.Ui/benchmarkcar.ui.react/src/index.js
+++ src/services/BenchmarkCar.Ui/benchmarkcar.ui.react/src/index.js
@@ -6,6 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import './i18n';
 
+require('dotenv').config()
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
