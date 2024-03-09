dd7a51691c8bce1560a018d3b7947ee5fb93284e
Guilherme Bley <guilhermecbley@gmail.com>
2024-03-08 23:58:27 -0300

    Fail in chokidar #16 - add env

--- src/services/BenchmarkCar.Ui/benchmarkcar.ui.react/src/index.js
+++ src/services/BenchmarkCar.Ui/benchmarkcar.ui.react/src/index.js
@@ -5,6 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import './i18n';
+import dotenv from 'dotenv';
+import path from 'path';
+
+// Configure dotenv
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
