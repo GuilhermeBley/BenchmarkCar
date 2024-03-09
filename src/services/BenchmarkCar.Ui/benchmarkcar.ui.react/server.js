748b194e1a4d7753d8ea5e3d8428762f676a0bcc
Guilherme Bley <guilhermecbley@gmail.com>
2024-03-08 23:35:49 -0300

    Fail in chokidar #16 - run on node

--- src/services/BenchmarkCar.Ui/benchmarkcar.ui.react/server.js
+++ src/services/BenchmarkCar.Ui/benchmarkcar.ui.react/server.js
@@ -1,16 +1,17 @@
-var express = require('express');
-
-var indexRoutes = require('./src/index');
+const express = require('express');
+const path = require('path');
 
 var app = express();
 
-app.use('/', indexRoutes);
+app.use(express.static(path.resolve(__dirname, './build')));
 
 // Allows you to set port in the project properties.
 app.set('port', process.env.PORT || 3000);
 
-var server = app.listen(app.get('port'), function() {
-    console.log('listening');
+const port = app.get('port');
+
+var server = app.listen(port, function() {
+    console.log(`listening in ${port}`);
 });
 
 module.exports = app
