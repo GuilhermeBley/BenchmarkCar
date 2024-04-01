7547ecfb7305c4602a5d10b9d785f3b347ea9456
Guilherme Bley <guilhermecbley@gmail.com>
2024-03-31 23:07:33 -0300

    array alert

--- src/services/BenchmarkCar.Ui/benchmarkcar.ui.react/src/components/alert/index.js
+++ src/services/BenchmarkCar.Ui/benchmarkcar.ui.react/src/components/alert/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css'
 
 /**
  * Top alerts messages
  * @param {Array} alerts 
  */
-const topAlert = (alerts, classCss = "alert-danger") => {
+function topAlert(alerts, classCss = "alert-danger") {
+
+    const [items, setItems] = useState(alerts);
+
+    /**
+     * @param {int} indexToRemove 
+     */
+    const removeArrayItem = (indexToRemove) => {
+        const newItems = items.filter((item, index) => index !== indexToRemove);
+        setItems(newItems);
+    }
 
     let completeClassCss = "alert d-block " + classCss;
 
-    return alerts.map((value, index) => (
+    let distinctArray = [...new Set(alerts)];
+
+    return distinctArray.slice(0, 3).map((value, index) => (
         <div class={completeClassCss} role="alert">
-            {value}
+            <div class="d-flex align-items-center">
+                {value}
+                <div class="ml-auto">
+                    <button class="btn btn-outline-danger border-0" onClick={removeArrayItem(index)}>x</button>
+                </div>
+            </div>
         </div>
     ));
 } 
